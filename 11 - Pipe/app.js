54976f29f9da343c2740678e2110fe1731cff6bf
Kaung San Kyaw <kskthedeveloper@gmail.com>
2017-10-13 23:01:30 +0630

    11-Pipe

---
+++ 11 - Pipe/app.js
@@ -0,0 +1,14 @@
+/*
+ * Stream
+ * Writable Stream -> to write data to Stream
+ * Readable Stream -> to read data from Stream
+ * Duplex -> can read and write to a Stream
+ */
+
+var http = require('http');
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
+
+myReadStream.pipe(myWriteStream);
