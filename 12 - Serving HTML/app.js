bbe5f03f02c4a524fd44731f1df9ec34ec99e582
Kaung San Kyaw <kskthedeveloper@gmail.com>
2017-10-14 11:10:56 +0630

    12 - Serving HTML

---
+++ 12 - Serving HTML/app.js
@@ -0,0 +1,18 @@
+/*
+ * Stream
+ * Writable Stream -> to write data to Stream
+ * Readable Stream -> to read data from Stream
+ * Duplex -> can read and write to a Stream
+ */
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Listening @ localhost:3000');
