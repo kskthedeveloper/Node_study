4e5c06895280b1f7ea44c9909f5136c9e113044e
kskthedeveloper <kskthedeveloper@gmail.com>
2017-10-15 01:30:22 -0400

    14- basic routing

--- 14-Basic/app.js
+++ 14-Basic/app.js
@@ -9,13 +9,21 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    var myObj = {
-        name: 'Kaung',
-        job: 'SE',
-        age: 24
-    };
-    res.end(JSON.stringify(myObj));
+  console.log('request was made: ' + req.url);
+  if (req.url === '/home' || req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if(req.url == '/contact'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if(req.url === '/api/ninjas') {
+    var ninjas = [{name: 'name', age: 29}, {name: 'name2', age: 30}];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 server.listen(3000, '0.0.0.0');
